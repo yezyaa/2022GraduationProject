e5b60622eb3cb845f3f14d32c71e5701ff6b5d27
yezyaa <iyeahji@gmail.com>
2022-11-12 17:45:08 +0900

    Player Move, Camera

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Transform characterBody;
+    [SerializeField] Transform cameraArm;
+    [SerializeField] float playerSpeed;
+
+    Animator animator;
+
+
+    void Start()
+    {
+        animator = characterBody.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        LookAround();
+        Move();
+    }
+
+    // 캐릭터 이동
+    void Move()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bool isMove = moveInput.magnitude != 0;
+        animator.SetBool("isMove", isMove);
+        if (isMove)
+        {
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+            Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
+            Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
+
+            characterBody.forward = moveDir;
+            transform.position += moveDir * Time.deltaTime * playerSpeed;
+        }
+    }
+
+
+    // 카메라 회전
+    void LookAround()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Vector3 camAngle = cameraArm.rotation.eulerAngles;
+        float x = camAngle.x - mouseDelta.y;
+
+        if (x < 180f)
+        {
+            x = Mathf.Clamp(x, -1f, 70f);
+        }
+        else
+        {
+            x = Mathf.Clamp(x, 355f, 361f);
+        }
+
+        cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
+    }
+}
